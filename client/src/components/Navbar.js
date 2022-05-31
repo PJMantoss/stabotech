b0ae2779bd72f918e1a6ff8c7d72e79cac85fcc6
PJMantoss <joelptoss@gmail.com>
2022-05-31 18:55:09 +0100

    update Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,10 +2,69 @@ import React from 'react';
 import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
 import Badge from '@mui/material/Badge';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+    height: 60px;
+    ${mobile({ height: "50px" })};
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+    ${mobile({ padding: "10px 0" })};
+`;
+
+const Language = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    ${mobile({ display: "none" })};
+`;
+
+const Left = styled.div`
+    display: flex;
+    align-items: center;
+    flex:1;
+`;
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+
+const Center = styled.div`
+    flex:1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+const Right = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    flex:1;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>Navbar</Container>
   )
 }
 
