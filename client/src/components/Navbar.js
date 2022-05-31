ee21bc99ca552a4dfecac4962acdf707d3e785f2
PJMantoss <joelptoss@gmail.com>
2022-05-31 18:46:51 +0100

    installed dependencies

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
 
 const Navbar = () => {
   return (
