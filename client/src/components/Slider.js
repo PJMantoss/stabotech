4d4813e8cd3abea2d5b88900e4d3bb04425bdfad
PJMantoss <joelptoss@gmail.com>
2022-06-01 14:34:42 +0100

    update Slider

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -4,9 +4,24 @@ import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 import { sliderItems } from '../data';
 import styled from 'styled-components';
 
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+`;
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${(props) => props.slideIndex * - 100}vw);
+`;
+
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <Container>Slider</Container>
   )
 }
 
