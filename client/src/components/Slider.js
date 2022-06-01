87ccc08eb402ff47794cf7bbee57ee41eacab0e5
PJMantoss <joelptoss@gmail.com>
2022-06-01 16:33:11 +0100

    update Slider

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -78,6 +78,15 @@ const Arrow = styled.div`
 
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = direction => {
+        if(direction === "left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex+1 : 0)
+        }
+    };
+
   return (
     <Container>
         <Arrow direction="left" onClick={() => handleClick("left")}>
