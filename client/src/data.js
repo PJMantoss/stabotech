9607c21f1b29ab3498b2f8e1f945b066a663d64f
PJMantoss <joelptoss@gmail.com>
2022-06-01 19:25:19 +0100

    update Slider

--- client/src/data.js
+++ client/src/data.js
@@ -1,14 +1,14 @@
 export const sliderItems = [
     {
         id: 1,
-        image: "https://www.nicepng.com/png/detail/322-3221261_green-n-white-dress-fashion-model.png",
+        image: "https://i.ibb.co/PDtNL1P/black-business-woman.png",
         title: "SUMMER SALE",
         desc: "Don't Compromise on Style. Get 30% Off!",
         bg: "f5fafd"
     },
     {
         id: 2,
-        image: "https://photos.google.com/photo/AF1QipOSIx7ru0kK1auOSlCf9nURLKxcvKXi-iGxYcbv",
+        image: "https://i.ibb.co/3dsTK7L/head-png-1000.png",
         title: "AUTUMN COLLECTION",
         desc: "Don't Compromise on Style. Get 30% Off!",
         bg: "fcf1ed"
