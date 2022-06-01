853cde5e44e7f2646f2fa93dd5c44ae9383e35bf
PJMantoss <joelptoss@gmail.com>
2022-06-01 16:26:51 +0100

    update App and Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 }
