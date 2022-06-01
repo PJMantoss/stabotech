8f8d667bfd02ec864fd77b291b975233e3b609d5
PJMantoss <joelptoss@gmail.com>
2022-06-01 14:32:40 +0100

    update Slider

---
+++ client/src/components/Slider.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from '../data';
+import styled from 'styled-components';
+
+const Slider = () => {
+  return (
+    <div>Slider</div>
+  )
+}
+
+export default Slider
---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        image: "https://www.nicepng.com/png/detail/322-3221261_green-n-white-dress-fashion-model.png",
+        title: "SUMMER SALE",
+        desc: "Don't Compromise on Style. Get 30% Off!",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        image: "https://i.ibb.co/DG69bQ4/2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "Don't Compromise on Style. Get 30% Off!",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        image: "https://i.ibb.co/cXFnLLV/3.png",
+        title: "WINTER COLLECTION",
+        desc: "Don't Compromise on Style. Get 30% Off!",
+        bg: "fbf0f4"
+    }
+];
