5bf032a29ca70f5d41367faffd206367f37aa0a4
PJMantoss <joelptoss@gmail.com>
2022-06-02 15:05:47 +0100

    add Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
