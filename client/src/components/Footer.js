b77f203fe4303ba44d5f5a4f186bd0cc1f7a7ef2
PJMantoss <joelptoss@gmail.com>
2022-06-02 15:07:30 +0100

    update Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,6 +9,76 @@ import EmailIcon from '@mui/icons-material/Email';
 
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+    display: inherit;
+    flex-direction: column;
+    flex: 1;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+    margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+  display: inherit;
+`;
+
+const SocialIcon = styled.div`
+  display: inherit;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #${props => props.color};
+  color: white;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  margin-top: 5px;
+`;
+
+const ContactItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
 const Footer = () => {
   return (
     <div>Footer</div>
