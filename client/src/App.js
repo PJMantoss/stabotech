018c71b477934752dee8695582b7376bc33d6440
PJMantoss <joelptoss@gmail.com>
2022-06-03 15:51:13 +0100

    update Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
---
+++ client/src/pages/Login.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0.5),
+    rgba(255, 255, 255, 0.5)
+  ), url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center;
+  background-size: cover;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  background: teal;
+  padding: 15px 20px;
+  margin: 10px 0;
+  color: white;
+  cursor: pointer;
+  &:disabled{
+    color: green;
+    cursor: not-allowed;
+  }
+`;
+
+const Error = styled.span`
+    color: red;
+`;
+
+const Link = styled.a`
+  font-size: 12px;
+  margin: 5px 0;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input 
+              placeholder="username"
+          />
+          <Input 
+              placeholder="password" 
+              type="password"
+          />
+          <Button>SIGN IN</Button>
+          {error && <Error>Something went wrong...</Error>}
+          <Link>FORGOT YOUR PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
