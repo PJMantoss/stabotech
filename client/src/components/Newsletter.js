40ff476a6a23ef7705d398d8d01a85be8a4c7467
PJMantoss <joelptoss@gmail.com>
2022-06-03 14:41:09 +0100

    update Newsletter

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -9,7 +9,7 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background: #fcf5f5;
+    margin: 30px auto;
 `;
 
 const Title = styled.h2`
