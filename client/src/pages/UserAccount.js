5314948621109834a446efedf46c9d37f864cba9
PJMantoss <joelptoss@gmail.com>
2022-06-04 13:25:55 +0100

    add UserProfile

---
+++ client/src/pages/UserAccount.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserAccount = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left></Left>
+
+            <Right></Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default UserAccount;
