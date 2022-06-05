8986f489bc80c22399c13b11be0251ee61167be0
PJMantoss <joelptoss@gmail.com>
2022-06-05 18:27:50 +0100

    update UserAccount

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import UserAccount from './pages/UserAccount';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <UserAccount />
-    </div>
+    </Router>
   );
 }
 
